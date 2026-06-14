4bc0a2af7b08441c03865e62d39176f73309710e
Roy Surya Pratama <139084505+royf5oil@users.noreply.github.com>
2026-06-14 16:37:47 +0700

    Refactor prime factorization logic and input handling
    Refactor the prime factorization program to improve readability and functionality. Adjusted input handling and fixed prime checking logic.

--- Project-FaktorisasiPrima/faktor_prima.py
+++ Project-FaktorisasiPrima/faktor_prima.py
@@ -1,41 +1,63 @@
 """ Program untuk mencari faktor prima
 dari suatu bilangan"""
 
-faktor_prima = []
-bil_prima = []
+print ("—"*40)
+print ("cari faktorisasi prima suatu bilangan")
+print ("—"*40)
+print ("Ketik: x untuk berhenti")
+print ("—"*40)
 
 def check_prima(n):
     if n <= 1:
         return n
     else:
-        for i in range(2,int(n**0.5)):
+        for i in range(2,int(n**0.5)+1):
             if n % i == 0:
                 return False
         return True
 
-def deret_prima(n):
-    for i in range(n+1):
-        if check_prima(i) == True:
-            bil_prima.append(i)
+def deret_prima(m):
+    bil_prima = []
+    for j in range(2,m+1):
+        if check_prima(j) == True:
+            bil_prima.append(j)
     return bil_prima        
 
+
 # Bilangan yang dicari
-def faktorisasi_prima(n):
-    n = int(input("Masukan bilangan: "))
+def faktorisasi_prima():
+    n = str(input("Masukan bilangan: "))
     temp = n
     a = 0
-
+   
+    if n == "x":
+        return 0
+    
+    faktor_prima = []
+    
+    result = deret_prima(int(n))
+    
     while a < len(result):
-        if n % bil_prima[a] == 0:
-            temp = n // bil_prima[a] 
+        if int(n) % result[a] == 0:
+            temp = int(n) // result[a] 
             n = temp
         
-            faktor_prima.append(bil_prima[a])
+            faktor_prima.append(str(result[a]))
         
-        elif n % bil_prima[a] != 0:
+        elif int(n) % result[a] != 0:
             a += 1
-    return faktor_prima
-            
-print (faktorisasi_prima(12))           
+    new = " x ".join(faktor_prima)
+    print (new)
+
+def note():
+    print ("—"*40)
+    print ("Terimakasih telah mengunakan program sederha ini!")    
+    
+while True:
+    if faktorisasi_prima() == 0:
+        note()
+        break
+    else:
+        faktorisasi_prima()
             
             
