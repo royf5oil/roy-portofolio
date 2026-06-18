aa71c7cd4d7fa8eae5147f10ab0aab859765acca
Roy Surya Pratama <139084505+royf5oil@users.noreply.github.com>
2026-06-18 19:56:16 +0700

    Implement difficulty levels in multiplication quiz
    Added difficulty levels and improved input handling for multiplication quiz.

--- Project-kuisPerkalian/kuis-perkalian.py
+++ Project-kuisPerkalian/kuis-perkalian.py
@@ -1,28 +1,58 @@
-print ("—"*10,"Kuis Perkalian","—"*10)
+import random as rd
+print ("—"*9,"Game Kuis Perkalian","—"*9)
 target = int(input("Perkalian Berapa?: "))
-count = int(input("Dari 1 sampai Berapa?: "))
 
-# Status
+print ("—"*11,"Level kesulitan","—"*11)
+print ("[1]Mudah")
+print ("[2]Sulit")
+
+
+# Status pemain
 result = 0
 correct = 0
 wrong = 0
 
+level = int(input("Level:"))
+if level == 1:
+    print ("ketik x, untuk berhenti")
+    print ("—"*23)
+    while True:
+        number = rd.randint(1,10)
+        result = number * target
+        user = str(input(f"{number} x {target} = "))
+        
+        if user.isnumeric():
+            if int(user) == result:
+                print ("Anda Benar!")
+                correct += 1
+            elif int(user) != result:
+                print ("Anda Salah!")  
+                wrong += 1  
+            else:
+                count += 1       
+        elif user == "x":
+            break   
+        
+elif level == 2:
+    print ("ketik x, untuk berhenti")
+    while True:
+        number = rd.randint(10,99)    
+        result = number * target
+        user = str(input(f"{number} x {target} = "))
+        
+        if user.isnumeric():
+            if int(user) == result:
+                print ("Anda Benar!")
+                correct += 1
+            elif int(user) != result:
+                print ("Anda Salah!")  
+                wrong += 1  
+            else:
+                count += 1     
+        elif user == "x":
+            break
 
-for i in range(1,count+1):
-    result = i * target
-    user = int(input(f"{i} x {target} = "))
-    
-    if user == result:
-        print ("Anda Benar!")
-        correct += 1
-    elif user != result:
-        print ("Anda Salah!")  
-        wrong += 1  
-    
-    else:
-        count += 1     
         
 print ("Total Benar:",correct)
 print ("Total Salah:",wrong)        
-    
-    
+print ("Programmer Quote: \"we are programmers, we create the digital world\"")   
